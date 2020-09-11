4e50c6f697646e028514b5f0fb434a37fe324cce
fimoefive <brothermj@hotmail.com>
2020-09-10 23:04:28 -0500

    I added an imported script to the ParkProvider.js and WeatherProvider.js files to make the key available for the get fetch call function and interpolated the keys into the http// link within the getfetch calls.

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -2,6 +2,10 @@ import { getWeather, useWeather } from "./WeatherProvider.js";
 import { WeatherHTML } from "./Weather.js";
 
 const eventHub = document.querySelector(".weatherContainer");
+eventHub.addEventListener("chosen", event => {
+    const zipCode = event.detail.parkZip;
+    weatherList(zipCode);
+});
 
 const render = (appWeather) => {
     const targetElement = document.querySelector(".weatherContainer");
@@ -10,12 +14,12 @@ const render = (appWeather) => {
         return WeatherHTML(weatherObj);
     })
     // console.log("HTMLArray", HTMLArray);
-    targetElement.innerHTML = HTMLArray.join("")
+    targetElement.innerHTML = HTMLArray.join("");
 };
 
 // Render weather forcast initally
-export const weatherList = () => {
-    getWeather()
+export const weatherList = (zipCode) => {
+    getWeather(zipCode)
         .then(() => {
             const weatherArray = useWeather();
             //console.log('weatherArray', weatherArray);
