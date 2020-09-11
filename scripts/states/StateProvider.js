1ae06c2af72c943d02965fe963b99f66c3e82342
Jennifer Starkey <jenniferrstarkey@yahoo.com>
2020-09-11 09:30:51 -0500

    added states array

--- scripts/states/StateProvider.js
+++ scripts/states/StateProvider.js
@@ -1,14 +1,54 @@
-let states = [];
+// Array of states
 
-export const useStates = () => {
-    return states.slice();
-}
-
-export const getStates = () => {
-    return fetch("")
-        .then(response => response.json())
-        .then(parsedStates => {
-            states = parsedStates
-        }
-    )
-}
+let states = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+]
