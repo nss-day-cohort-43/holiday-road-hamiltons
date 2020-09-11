6fa8bcabe3e300393e2a0270aae89ee3e2b6de22
Dani Crosby <dani.crosby1111@gmail.com>
2020-09-11 16:26:47 -0500

    tweaked states, need to fix bug on populate display

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,13 @@ import { eaterySelection } from "./eateries/EaterySelect.js"
 import {getParks,usePark} from './parks/ParkProvider.js'
 import {ParkSelect} from './parks/ParkSelect.js'
 import {weatherList} from './weather/WeatherList.js'
+import { selectState } from "./states/StateSelect.js"
+
 
 eaterySelection();
 attractionsSelection();
 getParks();
 usePark();
 ParkSelect();
-weatherList()
+weatherList();
+selectState()
