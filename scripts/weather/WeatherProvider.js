2817b907b9e28ae42d6c89a1a8f776dd43efc60c
fimoefive <brothermj@hotmail.com>
2020-09-13 23:54:58 -0500

    weatherProvider and List changes made to the EventListener and added an import of the usePark function in the WeatherProvider.js folder. Also added code to the getWeather function in the WeatherPovider.js file.

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,24 +1,28 @@
-import { keys } from "../Settings.js"
+import { keys } from "../Settings.js";
+import {usePark} from "../parks/ParkProvider.js";
 
 let weather = [];
 
-const eventHub = document.querySelector(".weatherContainer");
-
-const dispatchChangeEvent = () => {
-    const zipCodeChangedEvent = new CustomEvent("weatherChanged")
-
-    eventHub.dispatchEvent(zipCodeChangedEvent)
-};
-
 export const useWeather = () => {
-    // return weather.slice();
-};
+    return weather.slice();
+}
+
+// const eventHub = document.querySelector(".weatherContainer");
+// const dispatchChangeEvent = () => {
+//     const zipCodeChangedEvent = new CustomEvent("weatherChanged")
+//     eventHub.dispatchEvent(zipCodeChangedEvent)
+// };
 
 export const getWeather = (zipCode) => {
+    parks = usePark()
+    const parkTarget = parks.map(parkObj => {
+        return parkObj.name === document.querySelector(".parkPreview").value
+    })
+
     return fetch(`https://api.openweathermap.org/data/2.5/forecast?&units=imperial&zip=${zipCode}&appid=${keys.weatherKey}`)
         .then(response => response.json())
         .then(parsedWeather => {
-            weather = parsedWeather
+            weather = parsedWeather.list
         })
 };
 
