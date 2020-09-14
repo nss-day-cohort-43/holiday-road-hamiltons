ddb43df50d1f9db0a5fc027065c1227e3d2583a7
Zach Roark <zach.roark@yahoo.com>
2020-09-14 09:13:21 -0500

    new master

--- scripts/states/StateSelect.js
+++ scripts/states/StateSelect.js
@@ -1,5 +1,7 @@
 import { theStates } from "./StateProvider.js"
 
+let stateCollection = []
+
 const eventHub = document.querySelector(".dropdownContainer")
 const domElement = document.querySelector(".stateDrop")
 
@@ -8,8 +10,14 @@ export const selectState = () => {
     domElement.innerHTML = `
     <select class="dropdownContainer" id="selectState">
         <option value="0">Select a State</option>
+        ${stateCollection.map(stateObj => {
+            return`<option value="${stateObj}">${stateObj}</option>`
+        }).join("")
+    }
     </select>
     `
+
+
     const selectTarget = document.querySelector(".stateDrop")
     let stateHTMLRepresentation = "";
     const states = theStates()
@@ -20,6 +28,9 @@ export const selectState = () => {
 selectTarget.innerHTML += `
     ${stateHTMLRepresentation}
 `}
+
+
+
 
 const state = (stateObj) => {
     return `<option value="${stateObj}">${stateObj}</option>`
